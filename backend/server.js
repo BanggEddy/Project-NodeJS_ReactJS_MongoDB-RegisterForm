2f31f10b3d398e79eeaeaed5ddb51f47f70b38de
David LI <davidli4532@gmail.com>
2023-01-25 16:57:06 +0100

    backend finish

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+const cors = require('cors')
 
-app.listen(4000, console.log("Server 4000 Up"));
+mongoose.set("strictQuery", false);
+
+dotenv.config()
+
+mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database connected"))
+
+app.use(express.json())
+app.post('/', (req, res) => { //Faire un POST des infos 
+    res.send('POST request to the homepage')
+  })
+app.use(cors()); //Access to every port
+app.listen(4000, console.log("Server 4000 up"));
+
