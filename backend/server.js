62505d08b6c40c6e1380bcd3ddcae62bf1f0ffdd
David LI <davidli4532@gmail.com>
2023-01-26 11:29:02 +0100

    back-end update 0 error

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
+const routesUrls = require('./routes')
 const cors = require('cors')
 
 mongoose.set("strictQuery", false);
@@ -11,9 +12,7 @@ dotenv.config()
 mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database connected"))
 
 app.use(express.json())
-app.post('/', (req, res) => { //Faire un POST des infos 
-    res.send('POST request to the homepage')
-  })
-app.use(cors()); //Access to every port
-app.listen(4000, console.log("Server 4000 up"));
+app.use(cors());
+app.use('/app', routesUrls) // www.../app/signin
+app.listen(4000, () => console.log("Server 4000 up"));
 
