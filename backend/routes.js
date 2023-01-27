2a52605bbe66d3d53a76bca2566b0420577591a1
David LI <davidli4532@gmail.com>
2023-01-27 16:27:42 +0100

    hash & salt password finish

--- backend/routes.js
+++ backend/routes.js
@@ -1,14 +1,19 @@
 const express = require('express') //Export Express
 const SignUpTemplateCopy = require('./models/SignUpModel')
 const router = express.Router()
-
+const bcrypt = require('bcrypt')
 //.pot = Post Request
-router.post('/signup', (request, response) => { //When we're in signup page :
+router.post('/signup', async (request, response) => { //When we're in signup page :
+
+    const saltPassword = await bcrypt.genSalt(10) //Generate the salt by 10
+    const securePassword = await bcrypt.hash(request.body.password, saltPassword ) //salt and crypt the password the user send
+
     const signedUpUser = new SignUpTemplateCopy({ //When they submit, they create a new SignUp
+
         fullName:request.body.fullName, //grab the fullName of the request they enter (request = post)
         username:request.body.username,
         email:request.body.email,
-        password:request.body.password,
+        password:securePassword,
     })
     signedUpUser.save()//Save all data of user request
     .then(data => {
